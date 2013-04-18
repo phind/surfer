ab3d7aab48e034dbebc0beca050c931c4dd05c1d
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-04-18 09:05:50 -0500

    model -> ember data

--- client/lib/models/post.js
+++ client/lib/models/post.js
@@ -1,22 +1,35 @@
 (function (App) {
-    var posts = Ember.ArrayController.create({ content: [] });
-
-    $.ajax({
-        url: '/posts',
-        success: function (data) {
-            posts.addObjects(data.map(function (each) {
-                return Ember.Object.create(each);
-            }));
-        }
+    App.Store = DS.Store.extend({
+        revision: 12
     });
 
-    App.Post = Ember.Object.create({
-        find: function (arg) {
-            if (arg) {
-                return posts[arg];
-            }
 
-            return posts;
-        },
+    App.Post = DS.Model.extend({
+        placeName: DS.attr('string'),
+        title: DS.attr('string'),
+        content: DS.attr('string'),
+        updated: DS.attr('date'),
+        created: DS.attr('date'),
+        deadline: DS.attr('date'),
+//         tags: DS.attr('array'),
     });
+
+//     $.ajax({
+//         url: '/posts',
+//         success: function (data) {
+//             posts.addObjects(data.map(function (each) {
+//                 return Ember.Object.create(each);
+//             }));
+//         }
+//     });
+// 
+//     App.Post = Ember.Object.create({
+//         find: function (arg) {
+//             if (arg) {
+//                 return posts[arg];
+//             }
+// 
+//             return posts;
+//         },
+//     });
 })(window.App);
