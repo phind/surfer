8f2865170573c09f0f3272a2df9154a780bf71d5
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-04-19 08:19:51 -0500

    mixin already in its own file

--- client/lib/controllers/posts_controller.js
+++ client/lib/controllers/posts_controller.js
@@ -1,23 +1,3 @@
 (function (App) {
-    Ember.PaginationMixin = Ember.Mixin.create({
-        page: 0,
-        pageSize: 10,
-
-        pageCount: function () {
-
-            return Math.ceil(this.get('content.length') /
-                this.get('pageSize'));
-
-        }.property('content.length', 'pageSize'),
-
-        view: function () {
-            var pageSize = this.get('pageSize');
-            var start = this.get('page') * pageSize;
-            var end = start + pageSize;
-            return this.get('content').slice(start, end);
-
-        }.property('[]', 'page', 'pageSize')
-    });
-
     App.PostsController = Ember.ArrayController.extend(Ember.PaginationMixin);
 })(window.App);
