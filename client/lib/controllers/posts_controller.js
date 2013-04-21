06c464c4e9f8bb6600eaa23b163d772836d39613
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-04-20 21:01:17 -0500

    use pageablemixin

--- client/lib/controllers/posts_controller.js
+++ client/lib/controllers/posts_controller.js
@@ -1,3 +1,3 @@
 (function (App) {
-    App.PostsController = Ember.ArrayController.extend(Ember.PaginationMixin);
+    App.PostsController = Ember.ArrayController.extend(Ember.PageableMixin);
 })(window.App);
